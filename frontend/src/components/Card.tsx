c43924763603270fcd0f0f24bbfebc2f26f8f1da
Pjotr <pjotrzwartenkot@gmail.com>
2025-10-27 17:23:07 +0100

    Add Tailwind CSS integration and custom styles
    - Imported Tailwind CSS into the project.
    - Defined a custom font variable for 'Comic Neue'.
    - Created a new CSS class 'title-shadow' for enhanced text shadow effects.

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -3,49 +3,35 @@ import { useState } from "react";
 const Card = () => {
   const [isFlipped, setIsFlipped] = useState(false);
 
-  const perspectiveContainerStyle: React.CSSProperties = {
-    width: "300px",
-    height: "200px",
-    perspective: "2000px",
-  };
-
-  const flipCardInnerStyle: React.CSSProperties = {
-    width: "100%",
-    height: "100%",
-    transition: "transform 0.8s",
-    transformStyle: "preserve-3d",
-    transform: isFlipped ? "rotateY(180deg)" : "",
-  };
-
-  const flipCardFrontStyle: React.CSSProperties = {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    textAlign: "center",
-    borderRadius: "15px",
-    backfaceVisibility: "hidden",
-    backgroundColor: "#bbb",
-  };
-
-  const flipCardBackStyle: React.CSSProperties = {
-    ...flipCardFrontStyle,
-    transform: "rotateY(180deg)",
-  };
-
   return (
-    <>
-      <div style={perspectiveContainerStyle}>
-        <div style={flipCardInnerStyle}>
-          <div style={flipCardFrontStyle}>
-            <p>This is the front</p>
+    <div className="flex flex-col items-center gap-8">
+      <div className="w-[500px] h-[455px] perspective-[2000px]">
+        <div
+          className={`w-full h-full transition-transform duration-700 transform-3d ${
+            isFlipped ? "transform-[rotateY(180deg)]" : ""
+          }`}
+        >
+          <div className="absolute w-full h-full text-center backface-hidden rounded-4xl bg-[#F5F5F5] border-8 border-[#FFED48] drop-shadow-2xl pt-10 flex flex-col items-center gap-4">
+            <p className="font-comic font-bold text-3xl">Welk land is dit?</p>
+            <div className="w-102 mx-auto rounded-2xl overflow-hidden">
+              <img src="/images/nederland.png" className="w-full h-full" />
+            </div>
           </div>
-          <div style={flipCardBackStyle}>
-            <p>This is the back</p>
+          <div className="absolute w-full h-full text-center backface-hidden rounded-4xl bg-[#F5F5F5] transform-[rotateY(180deg)] border-8 border-[#4245FF] drop-shadow-2xl pt-10">
+            <p className="font-comic font-bold text-3xl">Nederland</p>
           </div>
         </div>
       </div>
-      <button onClick={() => setIsFlipped(!isFlipped)}>Flip</button>
-    </>
+      <button className="font-comic font-bold text-xl bg-[#C163D4] border-4 border-[#7A1C99] text-white rounded-full px-8 py-3 drop-shadow-xl">
+        Ezelsbruggetje
+      </button>
+      <button
+        onClick={() => setIsFlipped(!isFlipped)}
+        className="font-comic font-bold text-xl bg-[#FFF069] border-4 border-[#E8D100] rounded-full px-8 py-3 drop-shadow-xl"
+      >
+        Draai om
+      </button>
+    </div>
   );
 };
 
--- frontend/src/pages/Topo.tsx
+++ frontend/src/pages/Topo.tsx
@@ -2,9 +2,24 @@ import Card from "../components/Card";
 
 const Topo = () => {
   return (
-    <main>
-      <Card />
-    </main>
+    <div className="bg-[#D2F2FF] min-h-screen flex flex-col items-center gap-10 pt-8">
+      <header className="flex flex-col items-center gap-12 w-full px-4">
+        <div className="flex flex-col items-center gap-2">
+          <h1 className="text-6xl font-comic font-bold text-green-500 title-shadow">
+            Edutastic
+          </h1>
+          <h2 className="font-comic font-bold text-2xl">topografie</h2>
+        </div>
+
+        <div className="flex justify-between max-w-[370px] w-full">
+          <h2 className="font-comic font-bold text-xl">Vraag 1/10</h2>
+          <h2 className="font-comic font-bold text-xl">Punten: 0</h2>
+        </div>
+      </header>
+      <main>
+        <Card />
+      </main>
+    </div>
   );
 };
 
