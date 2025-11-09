d2dd250ca7642dc8650d2a245b55c833f17fd33c
Pjotr <pjotrzwartenkot@gmail.com>
2025-11-09 23:19:10 +0100

    Refactor getDefaultCountries to shuffle country data

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -12,80 +12,100 @@ export const sleep = (ms: number): Promise<void> =>
     }, ms);
   });
 
-export const getDefaultCountries = (): Array<Country> => {
-  return [
-    {
-      name: "nederland",
-      inhabitants: "18 miljoen",
-      capital: "Amsterdam",
-      donkeyBridgeDescription: "De leeuw staat symbool voor Nederland",
-    },
-    {
-      name: "chili",
-      inhabitants: "19 miljoen",
-      capital: "Santiago",
-      donkeyBridgeDescription:
-        "Chili is een lang en dun uitgerekt land, net als een chilipeper",
-    },
-    {
-      name: "duitsland",
-      inhabitants: "83 miljoen",
-      capital: "Berlijn",
-      donkeyBridgeDescription: "",
-    },
-    {
-      name: "china",
-      inhabitants: "1,4 miljard",
-      capital: "Beijing",
-      donkeyBridgeDescription: "",
-    },
-    {
-      name: "rusland",
-      inhabitants: "144 miljoen",
-      capital: "Moskou",
-      donkeyBridgeDescription: "",
-    },
-    {
-      name: "verenigde staten",
-      inhabitants: "331 miljoen",
-      capital: "Washington D.C.",
-      donkeyBridgeDescription: "",
-    },
-    {
-      name: "india",
-      inhabitants: "1,4 miljard",
-      capital: "New Delhi",
-      donkeyBridgeDescription: "",
-    },
-    {
-      name: "frankrijk",
-      inhabitants: "67 miljoen",
-      capital: "Parijs",
-      donkeyBridgeDescription: "",
-    },
-    {
-      name: "japan",
-      inhabitants: "125 miljoen",
-      capital: "Tokio",
-      donkeyBridgeDescription: "",
-    },
-    {
-      name: "brazilie",
-      inhabitants: "213 miljoen",
-      capital: "Brasília",
-      donkeyBridgeDescription: "",
-    },
-    {
-      name: "verenigd koninkrijk",
-      inhabitants: "67 miljoen",
-      capital: "Londen",
-      donkeyBridgeDescription: "",
-    },
-    {
-      name: "italie",
-      inhabitants: "60 miljoen",
-      capital: "Rome",
-      donkeyBridgeDescription: "",
-    },
-  ];
+const shuffle = (a: Array<unknown>) => {
+  let currentIndex = a.length;
+  while (currentIndex != 0) {
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [a[currentIndex], a[randomIndex]] = [a[randomIndex], a[currentIndex]];
+  }
 };
+
+const countries = [
+  {
+    name: "nederland",
+    inhabitants: "18 miljoen",
+    capital: "Amsterdam",
+    donkeyBridgeDescription: "De leeuw staat symbool voor Nederland",
+  },
+  {
+    name: "chili",
+    inhabitants: "19 miljoen",
+    capital: "Santiago",
+    donkeyBridgeDescription:
+      "Chili is een lang en dun uitgerekt land, net als een chilipeper",
+  },
+  {
+    name: "duitsland",
+    inhabitants: "83 miljoen",
+    capital: "Berlijn",
+    donkeyBridgeDescription:
+      "De adelaar kijkt naar het oosten, richting Duitsland",
+  },
+  {
+    name: "china",
+    inhabitants: "1,4 miljard",
+    capital: "Beijing",
+    donkeyBridgeDescription:
+      "De draak is het symbool van China en haar keizerlijke macht",
+  },
+  {
+    name: "rusland",
+    inhabitants: "144 miljoen",
+    capital: "Moskou",
+    donkeyBridgeDescription:
+      "De beer staat voor de kracht en grootte van Rusland",
+  },
+  {
+    name: "verenigde staten",
+    inhabitants: "331 miljoen",
+    capital: "Washington D.C.",
+    donkeyBridgeDescription:
+      "De kale adelaar (bald eagle) is het nationale symbool van de VS",
+  },
+  {
+    name: "india",
+    inhabitants: "1,4 miljard",
+    capital: "New Delhi",
+    donkeyBridgeDescription:
+      "De tijger is het nationale dier en symbool van India's natuur",
+  },
+  {
+    name: "frankrijk",
+    inhabitants: "67 miljoen",
+    capital: "Parijs",
+    donkeyBridgeDescription:
+      "De haan (le coq) kraait en is het symbool van Frankrijk",
+  },
+  {
+    name: "japan",
+    inhabitants: "125 miljoen",
+    capital: "Tokio",
+    donkeyBridgeDescription:
+      "De rijzende zon op de vlag: Japan = land van de rijzende zon",
+  },
+  {
+    name: "brazilie",
+    inhabitants: "213 miljoen",
+    capital: "Brasília",
+    donkeyBridgeDescription:
+      "De papegaai leeft in het Amazoneregenwoud van Brazilië",
+  },
+  {
+    name: "verenigd koninkrijk",
+    inhabitants: "67 miljoen",
+    capital: "Londen",
+    donkeyBridgeDescription: "De leeuw staat voor de Britse koninklijke macht",
+  },
+  {
+    name: "italie",
+    inhabitants: "60 miljoen",
+    capital: "Rome",
+    donkeyBridgeDescription: "Italië heeft de vorm van een laars.",
+  },
+];
+
+shuffle(countries);
+
+export const getDefaultCountries = (): Array<Country> => countries;
